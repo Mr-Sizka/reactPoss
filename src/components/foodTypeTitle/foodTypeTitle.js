5ec4f3181ca193dd2a77c37f4cd62cf70a5b7a26
code-piggy <works.sisuka@gmail.com>
2023-02-26 09:42:12 +0530

    2

--- src/components/foodTypeTitle/foodTypeTitle.js
+++ src/components/foodTypeTitle/foodTypeTitle.js
@@ -4,8 +4,12 @@ import breakfast_image from '../../assets/breakfast.png'
 
 const FoodTypeTitle = () => {
     return ( 
-        <div>
-            <img src={breakfast_image}></img>
+        <div className={classes.outer}>
+            <img className={classes.breakfast_img} src={breakfast_image}></img>
+            <div className={classes.titleHolder}>
+                <span className={classes.mainTitle}>Breackfast</span>
+                <span className={classes.subTitle}>13 Items</span></div>
+            
         </div>
      );
 }
